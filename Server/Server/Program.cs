2c173e580172b22c9f50263b2bb8bab0b1700bb3
Cas Koopmans <cjwkoopm@avans.nl>
2016-10-14 10:20:11 +0200

    Created Server

---
+++ Server/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using Server.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+        }
+
+        private TcpServer _tcpServer;
+
+        public Program()
+        {
+            _tcpServer = new TcpServer();
+
+            var serverThread = new Thread(_tcpServer.Run);
+            serverThread.Start();
+        }
+    }
+}
---
+++ Server/Server/Server/TcpServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server.Server
+{
+    class TcpServer
+    {
+        private readonly TcpListener _tcpListener;
+        public IPAddress IpAddress { get; set; }
+
+        public TcpServer()
+        {
+            IpAddress = GetLocalIpAddress();
+            _tcpListener = new TcpListener(IpAddress, 6969);
+            Console.WriteLine("IpAddress: {0}", IpAddress);
+        }
+
+        public void Run()
+        {
+        }
+
+        public static IPAddress GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip;
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
