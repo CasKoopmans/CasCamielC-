58a98d4459a659e61452267bc738fd4133ce6a80
camiel0910 <camiel0910@hotmail.com>
2016-10-14 11:04:23 +0200

    push because not saved

--- Server/Stratego/Character.cs
+++ Server/Stratego/Character.cs
@@ -8,5 +8,36 @@ namespace Stratego
 {
     class Character
     {
+        public int rank { get; }
+        public bool isBomb { get; }
+        public bool isFlag { get; }
+        public String name { get; }
+
+        public Character(int rank, bool isBomb, bool isFlag)
+        {
+            this.rank = rank;
+            this.isBomb = isBomb;
+            this.isFlag = isFlag;            
+            switch (rank)
+            {
+                case 1: name = "Spy";break;
+                case 2: name = "Scout";break;
+                case 3: name = "Miner";break;
+                case 4: name = "Sergeant";break;
+                case 5: name = "Lieutenant";break;
+                case 6: name = "Captain";break;
+                case 7: name = "Major";break;
+                case 8: name = "Colonel";break;
+                case 9: name = "General";break;
+                case 10: name = "Marshal";break;
+                default: Console.WriteLine("not a valid rank");break;
+            }
+            if (isBomb)
+                name = "Bomb";
+            if (isFlag)
+                name = "Flag";
+        }
+
+
     }
 }
--- Server/Stratego/Login.Designer.cs
+++ Server/Stratego/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stratego
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 439);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
