fe15b5d21702d3b7cbf2c6a91625935e19ffcdb5
camiel0910 <camiel0910@hotmail.com>
2016-11-02 11:55:33 +0100

    server weer toegevoegd

---
+++ Server/Server/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Client
+    {
+        String name { get; }
+        NetworkStream stream { get; }
+        public Client(String name, NetworkStream stream)
+        {
+            this.name = name;
+            this.stream = stream;
+        }
+    }
+}
