39a8f8980f35d253a3c4a18adf6f136722dc72b3
camiel0910 <camiel0910@hotmail.com>
2016-11-02 11:20:20 +0100

    server kant van login en register af

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -26,7 +27,63 @@ namespace Server
                 Console.WriteLine("Connection found");
                 NetworkStream stream = connection.GetStream();
                 String msg = readStream(stream);
+                String[] messages = msg.Split('_');
+                switch (messages[0])
+                {
+                    case "login": login(messages[1],stream);break;
+                    case "register": register(messages[1],stream);break;
+                    default: break;
+                }
             }
+        }
+
+        private bool login(String loginInfo,NetworkStream stream)
+        {
+            String[] namePassword = loginInfo.Split('-');
+            String name = namePassword[0];
+            String password = namePassword[1];
+
+            StreamReader sr = new StreamReader("login.txt");
+            while (sr.Peek() > 0)
+            {
+                if (sr.ReadLine().Equals(loginInfo))
+                {
+                    writeToStream(stream, "login_true");
+                    sr.Close();
+                    new Client(name, stream);                    
+                    return true;
+                }
+            }
+            sr.Close();
+            writeToStream(stream, "login_false");
+            return false;
+        }
+
+        private bool register(String registerInfo,NetworkStream stream)
+        {
+            String[] namePassword = registerInfo.Split('-');
+            String name = namePassword[0];
+            String password = namePassword[1];
+
+            StreamReader sr = new StreamReader("login.txt");
+            while (sr.Peek() > 0)
+            {
+                String otherName = sr.ReadLine().Split('-')[0];
+                if (otherName.Equals(name))
+                {
+                    writeToStream(stream, "register_false");
+                    sr.Close();
+                    return false;
+                }
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("login.txt",true);
+            sw.WriteLine(registerInfo);
+            sw.Close();
+            writeToStream(stream, "register_true");
+            new Client(name, stream);
+            return true;
         }
 
         private String readStream(NetworkStream stream)
@@ -44,7 +101,11 @@ namespace Server
         {
             Byte[] msgLenght = new Byte[4];
             msgLenght = BitConverter.GetBytes(msg.Length);
-            stream.Write 
+            stream.Write(msgLenght,0,4);
+
+            Byte[] finalMsg = new Byte[msg.Length];
+            finalMsg = Encoding.ASCII.GetBytes(msg);
+            stream.Write(finalMsg, 0, msg.Length);
         }
 
     }
