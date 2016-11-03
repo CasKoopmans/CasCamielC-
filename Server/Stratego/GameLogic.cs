0442b117814bd6df30238496d24279bc04fef545
camiel0910 <camiel0910@hotmail.com>
2016-11-03 14:20:57 +0100

    commited gamelogic for errors

--- Server/Stratego/GameLogic.cs
+++ Server/Stratego/GameLogic.cs
@@ -8,80 +8,83 @@ namespace Stratego
 {
     class GameLogic
     {
-        private Board board;
-        public GameLogic(Board board)
-        {
-            this.board = board;
-        }
+    //    public List<Character> characters;
+    //    public GameLogic()
+    //    {
 
+    //    }
 
+    //    public void updateCharacters(List<Character> c)
+    //    {
+    //        characters = c;
+    //    }
+        
+    //    public void fight(Character attacker, Character defender)
+    //     {
 
-        public void fight(Character attacker, Character defender)
-        {
+    //        switch (attacker.name)
+    //        {
+    //            case "Spy":
+    //                {
+    //                    if (defender.name == "Marshall")
+    //                    {
+    //                        //board.deleteEnemyCharacter(defender);
+    //                        return;
+    //                    }
+    //                }
+    //                break;
+    //            case "Miner":
+    //                {
+    //                    if (defender.name == "Bomb")
+    //                    {
+    //                        //board.deleteEnemyCharacter(defender);
+    //                        return;
+    //                    }
+    //                }
+    //                break;
+    //            default:
+    //                break;
+    //        }
+    //        switch (defender.name)
+    //        {
+    //            case "Bomb":
+    //               // board.deleteCharacter(attacker);
+    //                break;
+    //            case "Flag":
+    //                Console.WriteLine("YOU WIN");
+    //                break;
+    //            default:
+    //                {
+    //                    if (attacker.rank > defender.rank)
+    //                        //board.deleteEnemyCharacter(defender);
 
-            switch (attacker.name)
-            {
-                case "Spy":
-                    {
-                        if (defender.name == "Marshall")
-                        {
-                            board.deleteEnemyCharacter(defender);
-                            return;
-                        }
-                    }
-                    break;
-                case "Miner":
-                    {
-                        if (defender.name == "Bomb")
-                        {
-                            board.deleteEnemyCharacter(defender);
-                            return;
-                        }
-                    }
-                    break;
-                default:
-                    break;
-            }
-            switch (defender.name)
-            {
-                case "Bomb":
-                    board.deleteCharacter(attacker);
-                    break;
-                case "Flag":
-                    Console.WriteLine("YOU WIN");
-                    break;
-                default:
-                    {
-                        if (attacker.rank > defender.rank)
-                            board.deleteEnemyCharacter(defender);
+    //                    else if (attacker.rank < defender.rank)
+    //                            //board.deleteCharacter(attacker);
 
-                        else if (attacker.rank < defender.rank)
-                                board.deleteCharacter(attacker);
+    //                    else if (attacker.rank == defender.rank)
+    //                        {
+    //                            //board.deleteCharacter(attacker);
+    //                           // board.deleteEnemyCharacter(defender);
+    //                        }
+    //                }
+    //                break;
+    //        }
+    //    }
 
-                        else if (attacker.rank == defender.rank)
-                            {
-                                board.deleteCharacter(attacker);
-                                board.deleteEnemyCharacter(defender);
-                            }
-                    }
-                    break;
-            }
-        }
+    //    public bool checkAvailable(Character character, int targetX, int targetY)
+    //    {
+    //        int x = character.x;
+    //        int y = character.y;
 
-        public bool checkAvailable(Character character, int targetX, int targetY)
-        {
-            int x = character.x;
-            int y = character.y;
+    //        foreach (Character other in board.getAllCharacters())
+    //        {
+    //            if (targetX == other.x && targetY == other.y)
+    //            {
+    //                return false;
+    //            }
+    //        }
 
-            foreach (Character other in board.getAllCharacters())
-            {
-                if (targetX == other.x && targetY == other.y)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+    //        return true;
+    //    }
     }
 }
